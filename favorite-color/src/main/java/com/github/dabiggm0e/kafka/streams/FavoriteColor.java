364bacfa47783cb5645739a3264d5ddec72b6219
moe <dabiggmoe@gmail.com>
2020-02-21 02:37:22 +0300

    fixed reading from intermediary topic from as a stream to as a table

--- favorite-color/src/main/java/com/github/dabiggm0e/kafka/streams/FavoriteColor.java
+++ favorite-color/src/main/java/com/github/dabiggm0e/kafka/streams/FavoriteColor.java
@@ -3,6 +3,7 @@ package com.github.dabiggm0e.kafka.streams;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
@@ -14,9 +15,9 @@ import java.util.Properties;
 public class FavoriteColor {
     static  String applicationId = "word-count";
     static String bootstrapServers = "localhost:9092";
-    static String inputTopic = "streams-favcolor-input";
-    static String intermediaryTopic = "streams-favcolor-input-compacted";
-    static String outputTopic = "streams-favcolor-output";
+    static String inputTopic = "favcolor-input";
+    static String intermediaryTopic = "favcolor-input-compacted";
+    static String outputTopic = "favcolor-output";
 
 
     static Properties getStreamsConfig() {
@@ -34,20 +35,22 @@ public class FavoriteColor {
         // stream from kakfa
         List<String> allowedColors = Arrays.asList("red", "green", "blue");
 
-        KStream<String, String> favColorInput = builder.stream(inputTopic);
-        KStream<String, String> allowedFavColorInput = favColorInput
-                .filter((key, value) -> allowedColors.contains(value.toLowerCase()));
 
-        KTable<String, String> reducedFavColorTable = allowedFavColorInput
-                .groupByKey()
-                .reduce((oldValue, newValue) -> newValue);
+        KStream<String, String> textLines = builder.stream(inputTopic);
 
-        KStream<String, String>  favColorFilteredStream = reducedFavColorTable.toStream();
+        KStream<String, String> usersAndColors = textLines
+                .filter((key, value) -> value.contains(","))
+                .selectKey((key, value)-> value.split(",")[0].toLowerCase())
+                .mapValues(value -> value.split(",")[1].toLowerCase())
+                .filter((user, color) -> allowedColors.contains(color));
 
-        KTable<String, Long> favColorOutput = favColorFilteredStream
-                .selectKey((key, value)->value.toLowerCase())
-                .groupByKey()
-                .count();
+        usersAndColors.to(intermediaryTopic);
+
+        KTable<String, String> usersAndColorsTable = builder.table(intermediaryTopic);
+        KTable<String, Long> favColorOutput = usersAndColorsTable
+                .groupBy((user, color)-> new KeyValue<>(color, color))
+                .count(Named.as("CountsByColor"));
+
 
         favColorOutput.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
         // write back to kafka
