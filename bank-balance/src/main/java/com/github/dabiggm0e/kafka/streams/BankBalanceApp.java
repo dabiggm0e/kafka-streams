62e20bc1c4b3246ea888810db4d979e456c07d9f
moe <dabiggmoe@gmail.com>
2020-02-22 06:21:53 +0300

    output topic now writes key(customer),value(balance,count,time)

--- bank-balance/src/main/java/com/github/dabiggm0e/kafka/streams/BankBalanceApp.java
+++ bank-balance/src/main/java/com/github/dabiggm0e/kafka/streams/BankBalanceApp.java
@@ -1,30 +1,39 @@
 package com.github.dabiggm0e.kafka.streams;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Named;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.text.DecimalFormat;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
+
+import static org.apache.kafka.streams.kstream.Materialized.as;
 
 public class BankBalanceApp {
 
     static  String applicationId = "bank-balance";
     static String bootstrapServers = "localhost:9092";
     static String inputTopic = "bank-transactions";
-    static String outputTopic = "bank-accounts";
+    static String outputTopic = "bank-accounts2";
 
     static JsonParser jsonParser = new JsonParser();
 
@@ -111,24 +120,75 @@ public class BankBalanceApp {
         return transaction;
     }
 
+    static String newBalance(String transactionString, String balanceString) {
+        JsonParser jsonParser = new JsonParser();
+
+        JsonObject transaction = jsonParser.parse(transactionString).getAsJsonObject();
+        JsonObject balance = jsonParser.parse(balanceString).getAsJsonObject();
+
+        JsonObject newBalance = new JsonObject();
+        newBalance.addProperty("count", balance.get("count").getAsInt()+1);
+        newBalance.addProperty("balance",
+                transaction.get("amount").getAsDouble() + balance.get("balance").getAsDouble());
+
+        Long balanceEpoch = Instant.parse(balance.get("time").getAsString()).toEpochMilli();
+        Long transactionEpoch = null;
+        Instant newBalanceInstant = Instant.now();
+        try {
+            transactionEpoch = Instant.parse(transaction.get("time").getAsString()).toEpochMilli();
+            newBalanceInstant = Instant.ofEpochMilli(Math.max(balanceEpoch, transactionEpoch));
+
+        } catch (Exception e) {
+            System.out.println("Transaction time: " + transaction.get("time").getAsString() );
+            System.out.println("Balance time: " + balance.get("time").getAsString() );
+            e.printStackTrace();
+        }
+        newBalance.addProperty("time", newBalanceInstant.toString());
+        return newBalance.toString();
+
+    }
     static void createBankBalanceStream(final StreamsBuilder builder) {
         // stream from kakfa
 
+        // json Serde
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
 
-        KStream<String, String> transactionsStream = builder.stream(inputTopic);
+        JsonObject initialBalance = new JsonObject();
+        initialBalance.addProperty("balance", 0d);
+        initialBalance.addProperty("count", 0);
+        initialBalance.addProperty("time", Instant.ofEpochMilli(0L).toString());
+        String initialBalanceString = initialBalance.toString();
+
+
+        KStream<String, String> transactionsStream = builder.stream(
+                inputTopic/*,
+                Consumed.with(Serdes.String(),jsonSerde)*/);
+
+
         KStream<String, String> filteredTransactionsStream = transactionsStream
                 .filterNot((key, value) -> parseCustomerName(value).equals("") );// filter out transactions without names
 
         KTable<String, String> balancesTable = filteredTransactionsStream
-                .selectKey((key, value) -> parseCustomerName(value)) // select customer name as key in case key wasn't populated by the producer
-                .groupByKey()
-                .reduce(
-                        (oldValue, newValue) -> getTransactionJson(
-                                parseCustomerName(newValue),
-                                parseTransactionAmount(oldValue) + parseTransactionAmount(newValue),
-                                parseTransactionTime(newValue)
-                        ).toString()
-                );
+                .selectKey((key, value) -> parseCustomerName(value.toString())) // select customer name as key in case key wasn't populated by the producer
+                .groupByKey(
+                     /*   Grouped.with(Serdes.String(), jsonSerde)*/)
+                .aggregate(new Initializer<String>() {
+                    @Override
+                    public String apply() {
+                        return initialBalanceString.toString();
+                    }
+                }, new Aggregator<String, String, String>() {
+                    @Override
+                    public String apply(final String key, final String transaction,final String balance) {
+                        return newBalance(transaction, balance).toString();
+                    }
+                },Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as(outputTopic));
+             //   );
+                        //Materialized.<String, JsonObject, KeyValueStore<Bytes, byte[]>>as("counts-store"));
+
+
 
         balancesTable.toStream().to(
                 outputTopic,
